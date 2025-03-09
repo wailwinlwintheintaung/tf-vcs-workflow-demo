2c8a664104458a957970db131e4f3ed26435ad43
wailwinlwintheintaung <wailwinlwintheintaung@gmail.com>
2025-03-09 18:00:09 +0800

    create second vpc

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ resource "aws_vpc" "hello_vpc" {
   enable_dns_support   = true
 
 }
+
+resource "aws_vpc" "hello_vpc_2" {
+
+  cidr_block           = "192.168.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+}
--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "vpc_id" {
   value       = aws_vpc.hello_vpc.id
   description = "VPC ID."
 }
+
+output "vpc_id" {
+  value       = aws_vpc.hello_vpc_2.id
+  description = "VPC ID."
+}
